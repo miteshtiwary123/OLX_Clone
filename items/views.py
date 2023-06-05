5f0ce65e0c2e4b60b21cab07dd0c53bd15ba1298
Mitesh Kumar Tiwary <miteshtiwary123@users.noreply.github.com>
2023-06-05 16:26:42 +0530

    Edit button created

--- items/views.py
+++ items/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404, redirect
-from .forms import NewItemForm
+from .forms import NewItemForm, EditItemForm
 from .models import Item
 
 def detail(request, pk):
@@ -29,6 +29,24 @@ def new(request):
     return render(request, 'item/form.html', {
         'form': form,
         'title': 'New Item',
+    })
+
+@login_required
+def edit(request, pk):
+    item = get_object_or_404(Item, pk=pk, created_by=request.user)
+    if request.method == 'POST':
+        form = EditItemForm(request.POST, request.FILES, instance=item)
+
+        if form.is_valid():
+            form.save()
+
+            return redirect('items:detail', pk=item.id)
+    else:
+        form = EditItemForm(instance=item)
+
+    return render(request, 'item/form.html', {
+        'form': form,
+        'title': 'Edit Item',
     })
 
 @login_required
